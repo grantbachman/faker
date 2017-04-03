e92b83d2fa07f8e05109b102b85f7b801c037272
Grant David Bachman <grantbachman@gmail.com>
2017-04-03 07:55:58 -0400

    Move internet tests out of factory module

--- tests/factory.py
+++ tests/factory.py
@@ -285,45 +285,6 @@ class FactoryTestCase(unittest.TestCase):
             email = factory.email()
             self.assertTrue('@' in email)
 
-    def test_ipv4(self):
-        from faker.providers.internet import Provider
-
-        provider = Provider(None)
-
-        for _ in range(999):
-            address = provider.ipv4()
-            self.assertTrue(len(address) >= 7)
-            self.assertTrue(len(address) <= 15)
-            self.assertTrue(
-                re.compile(r'^(\d{1,3}\.){3}\d{1,3}$').search(address))
-
-        for _ in range(999):
-            address = provider.ipv4(network=True)
-            self.assertTrue(len(address) >= 9)
-            self.assertTrue(len(address) <= 18)
-            self.assertTrue(
-                re.compile(r'^(\d{1,3}\.){3}\d{1,3}/\d{1,2}$').search(address))
-
-    def test_ipv6(self):
-        from faker.providers.internet import Provider
-
-        provider = Provider(None)
-
-        for _ in range(999):
-            address = provider.ipv6()
-            self.assertTrue(len(address) >= 3)  # ::1
-            self.assertTrue(len(address) <= 39)
-            self.assertTrue(
-                re.compile(r'^([0-9a-f]{0,4}:){2,7}[0-9a-f]{1,4}$').search(address))
-
-        for _ in range(999):
-            address = provider.ipv6(network=True)
-            self.assertTrue(len(address) >= 4)  # ::/8
-            self.assertTrue(len(address) <= 39 + 4)
-            self.assertTrue(
-                re.compile(r'^([0-9a-f]{0,4}:){2,7}[0-9a-f]{0,4}/\d{1,3}$').search(
-                    address))
-
     def test_random_sample_unique(self):
         from faker.providers import BaseProvider
         provider = BaseProvider(None)
--- tests/providers/internet.py
+++ tests/providers/internet.py
@@ -6,10 +6,47 @@ import unittest
 
 from email_validator import validate_email
 
+import re
 from faker import Factory
 from faker.providers.person.ja_JP import Provider as JaProvider
+from faker.providers.internet import Provider
 from faker.utils import text
 from .. import string_types
+
+
+class TestBaseInternet(unittest.TestCase):
+
+    def test_ipv4(self):
+        provider = Provider(None)
+
+        for _ in range(999):
+            address = provider.ipv4()
+            self.assertTrue(len(address) >= 7)
+            self.assertTrue(len(address) <= 15)
+            self.assertTrue(
+                re.compile(r'^(\d{1,3}\.){3}\d{1,3}$').search(address))
+
+        for _ in range(999):
+            address = provider.ipv4(network=True)
+            self.assertTrue(len(address) >= 9)
+            self.assertTrue(len(address) <= 18)
+            self.assertTrue(
+                re.compile(r'^(\d{1,3}\.){3}\d{1,3}/\d{1,2}$').search(address))
+
+    def test_ipv6(self):
+        provider = Provider(None)
+
+        for _ in range(999):
+            address = provider.ipv6()
+            self.assertTrue(len(address) >= 3)  # ::1
+            self.assertTrue(len(address) <= 39)
+            self.assertTrue(re.compile(r'^([0-9a-f]{0,4}:){2,7}[0-9a-f]{1,4}$').search(address))
+
+        for _ in range(999):
+            address = provider.ipv6(network=True)
+            self.assertTrue(len(address) >= 4)  # ::/8
+            self.assertTrue(len(address) <= 39 + 4)
+            self.assertTrue(re.compile(r'^([0-9a-f]{0,4}:){2,7}[0-9a-f]{0,4}/\d{1,3}$').search(address))
 
 
 class TestJaJP(unittest.TestCase):
